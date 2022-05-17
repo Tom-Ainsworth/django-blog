f348cdccd8bb3c077acf07723e047d2ad3b73877
Tom Ainsworth <tomainsworth94@gmail.com>
2022-05-17 15:02:24 +0100

    Setup Heroku app ready for deployment

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ[
+    "DATABASE_URL"
+] = "postgres://zsseelfiphpqov:f9a754152c2dc9608d297ccb5b7f9e430f31593901eeea94b96a3c837f730c0a@ec2-52-212-228-71.eu-west-1.compute.amazonaws.com:5432/d65569bk19lvdu"
+os.environ["SECRET_KEY"] = "t&xx8zr48573z07p8e3f+da8)2h6%lwd@0!2-vg9#bgc54^iek"
