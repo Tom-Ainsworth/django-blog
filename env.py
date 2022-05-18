d5403ad6e7cd23bc02828fb10eecbccb1119f847
Tom Ainsworth <tomainsworth94@gmail.com>
2022-05-18 11:23:43 +0100

    deployment commit

--- env.py
+++ env.py
@@ -4,3 +4,4 @@ os.environ[
     "DATABASE_URL"
 ] = "postgres://zsseelfiphpqov:f9a754152c2dc9608d297ccb5b7f9e430f31593901eeea94b96a3c837f730c0a@ec2-52-212-228-71.eu-west-1.compute.amazonaws.com:5432/d65569bk19lvdu"
 os.environ["SECRET_KEY"] = "t&xx8zr48573z07p8e3f+da8)2h6%lwd@0!2-vg9#bgc54^iek"
+os.environ["CLOUDINARY_URL"] = "cloudinary://428236463679999:1J0Zaq2WV9SpDMuqLE-EXX1xkz0@dirub6uda"
