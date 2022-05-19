2dfab80de8968a197fd275124b23e4f69f91ed05
Tom Ainsworth <tomainsworth94@gmail.com>
2022-05-19 18:17:26 +0100

    Remove env.py

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ["DATABASE_URL"] = "postgres://crhobdoftoecai:f17e8826f2090d913a83fbb65731eb887a0c50b758834d045cc96265210ed077@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/dbt7vg1ettdu11"
-os.environ["SECRET_KEY"] = "t&xx8zr48573z07p8e3f+da8)2h6%lwd@0!2-vg9#bgc54^iek"
-os.environ["CLOUDINARY_URL"] = "cloudinary://428236463679999:1J0Zaq2WV9SpDMuqLE-EXX1xkz0@dirub6uda"
